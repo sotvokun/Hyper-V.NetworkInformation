7cbf21d65b43f6b61e61767591839f68b94cbae0
Sotvokun <sotvokun@outlook.com>
2019-05-31 21:54:33 +0800

    First commit

---
+++ Hyper-V.NetworkInformation.psm1
@@ -0,0 +1,62 @@
+﻿<#
+ .SYNOPSIS
+ Gets the IP and MAC address of running VMs.
+
+ .DESCRIPTION
+ Gets and displays Hyper-V VMs' IP and MAC address which they are running.
+
+ .Parameter Name
+ The IP and MAC address by the specific name of VM.
+
+ .OUTPUTS
+ The list of objects about VMs' network information.
+
+ .EXAMPLE
+ Get-VMNetworkInformation
+
+ .EXAMPLE
+ Get-VMNetworkInformation -Name "vm1"
+#>
+function Get-VMNetworkInformation()
+{
+param(
+    [string]$Name = ""
+)
+
+    $VM_macs = @{}
+    $VM_ips = @()
+    
+    #Get the VMs' mac addresses
+    Get-VM | ForEach-Object {
+        $mac_wo_line = ($_.NetworkAdapters | Select -ExpandProperty MacAddress).ToLower()
+        $a = $mac_wo_line -Split '(\w{2})' | Where  -Property Length -NE 0
+        $VM_macs[$_.name]=($a -join "-")
+    }
+    
+    #Get all IPs in LAN by `arp'
+    arp -a | ForEach-Object {
+        $line = $_
+        foreach($i in $VM_macs.GetEnumerator())
+        {
+            if($line.Contains($i.Value))
+            {
+                $result = [pscustomobject]@{
+                    Name = $i.Key
+                    IP = ((-split $line)[0])
+                    MACAddress = $i.Value
+                }
+                $VM_ips += $result
+            }
+        }
+    }
+
+    if($Name.Length -ne 0)
+    {
+        return ($VM_ips | Where -Property Name -EQ $Name)
+    }
+    else
+    {
+        return $VM_ips
+    }
+}
+Export-ModuleMember -Function Get-VMNetworkInformation
---
+++ README.md
@@ -0,0 +1,12 @@
+# Hyper-V.NetworkInformation
+
+If you use Hyper-V like a remote server and use ssh to access it. Thus when your VM used vEthernet (Default Switch)(or another switch) and operating system weren't Microsoft Windows, that to know the IP address of VM is an inconvenient thing.
+
+The Hyper-V module provided some useful command, and use  Windows utility program combinedly can know the IP of the running virtual machines。
+
+Usage
+
+1. Please make sure you are installed the Hyper-V Completely.
+2. Please make sure you are in Hyper-V administrator user group.
+3. [Install the module](https://docs.microsoft.com/en-us/powershell/developer/module/installing-a-powershell-module).
+4. [Import](https://docs.microsoft.com/en-us/powershell/developer/module/importing-a-powershell-module) this module and enjoy it.
